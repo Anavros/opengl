2fbbbef1008aaf25871c6ad3f80e9f754db7edf9
Anavros <jtdobbs1@me.com>
2017-03-30 16:10:10 -0400

    Improved shader generation functions.

---
+++ alias.sh
@@ -0,0 +1,3 @@
+
+alias r='make run'
+alias c='make'
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// This is the magic right here.
 #define GL_GLEXT_PROTOTYPES
 #include <GLFW/glfw3.h>
 
@@ -35,35 +36,29 @@ char* load(const char* filename) {
 
 
 void error_callback(int error, const char* description) {
+    (void)error;
     fprintf(stderr, "Error: %s\n", description);
 }
 
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    (void)scancode;
+    (void)mods;
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         puts("Pressed!");
         glfwSetWindowShouldClose(window, 1);
     }
 }
 
-typedef struct Attributes Attributes;
-struct Attributes {
-    float x, y;
-};
 
-const Attributes vertices[3] =
-    { {-1.0f, -1.0f }
-    , { 1.0f, -1.0f }
-    , { 1.0f,  1.0f }
-};
-
-
-void run(GLFWwindow* window, GLuint program) {
+void run(GLFWwindow* window, GLuint program, GLuint verts) {
     glfwSetKeyCallback(window, key_callback);
     while (!glfwWindowShouldClose(window)) {
         int w, h;
         glfwGetFramebufferSize(window, &w, &h);
-        //float ratio = w / (float) h;
+        float ratio = w / (float) h;
+        (void)ratio;
+        (void)verts;
         glViewport(0, 0, w, h);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -75,26 +70,66 @@ void run(GLFWwindow* window, GLuint program) {
     }
 }
 
+GLfloat vertices[] =
+    { -0.5f, -0.5f
+    ,  0.5f, -0.5f
+    ,  0.5f,  0.5f
+    };
 
-GLuint gen_buffer(Attributes* attrs) {
+
+GLuint gen_buffer(GLfloat *verts) {
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(attrs), attrs, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
     return buffer;
 }
 
 
-GLuint gen_shader(const char *text, int type) {
-    // 'type' should be either GL_VERTEX_SHADER or GL_FRAGMENT_SHADER.
-    GLuint shader = glCreateShader(type);
+GLuint gen_shader(const char *text, GLuint shader) {
     glShaderSource(shader, 1, &text, NULL);
     // TODO: include error checking.
     glCompileShader(shader);
+
+    // Error checking.
+    GLint err = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &err);
+    if (err) {
+        //GLsizei log_size = 0;
+        GLchar log[512];
+        puts("Shader compilation error.");
+        /*
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_size);
+        printf("LOG SIZE: %d\n", log_size);
+        log = malloc(sizeof(GLchar) * log_size);
+        if (!log) {
+            puts("Malloc failed! Panic!");
+            return 0;
+        }
+        glGetShaderInfoLog(shader, log_size, NULL, log);
+        */
+        glGetShaderInfoLog(shader, 512, NULL, log);
+        fprintf(stderr, "%s\n", log);
+        glDeleteShader(shader);
+        return 0;
+    }
+
     // Remember that GL objects are just uint identifiers.
     // Everything stays on the graphics card.
     // Returning the 'shader' is just returning its handle.
     return shader;
+}
+
+
+GLuint gen_v_shader(const char *text) {
+    GLuint shader = glCreateShader(GL_VERTEX_SHADER);
+    return gen_shader(text, shader);
+}
+
+
+GLuint gen_f_shader(const char *text) {
+    GLuint shader = glCreateShader(GL_FRAGMENT_SHADER);
+    return gen_shader(text, shader);
 }
 
 
@@ -119,12 +154,13 @@ int main(void) {
     glfwMakeContextCurrent(window);
 
     //GLuint attributes, vs, fs, program;
-    GLuint vs = gen_shader(load(V_SHADER), GL_VERTEX_SHADER);
-    GLuint fs = gen_shader(load(F_SHADER), GL_FRAGMENT_SHADER);
+    GLuint vs = gen_v_shader(load(V_SHADER));
+    GLuint fs = gen_f_shader(load(F_SHADER));
     GLuint program = gen_program(vs, fs);
+    GLuint verts = gen_buffer(vertices); // global vertices
 
 
-    run(window, program);
+    run(window, program, verts);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
